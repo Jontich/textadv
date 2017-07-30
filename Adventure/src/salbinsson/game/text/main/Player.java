36415d60839b61763ff43ad085740c027c72727c
staldeh <s_albinsson@yahoo.co.uk>
2017-07-30 18:17:38 +0200

    New stuff

--- Adventure/src/salbinsson/game/text/main/Player.java
+++ Adventure/src/salbinsson/game/text/main/Player.java
@@ -3,8 +3,11 @@ import salbinsson.game.text.stats.*;
 public class Player {
 	Accuracy acc = new Accuracy();
 	String Name = "Default";
+	enum Friendly {good, bad, ugly}; 
+	
 	
 	Player (int hit, int health, String N) {
+			Friendly F = Friendly.good;
 			acc.hit = hit;
 			acc.health = health;
 			Name = N;
---
+++ Adventure/src/salbinsson/game/text/main/PlayerList.java
@@ -0,0 +1,8 @@
+package salbinsson.game.text.main;
+
+public class PlayerList {
+	PlayerList (int Size){
+		Player[] Players = new Player[Size];
+	}
+
+}
